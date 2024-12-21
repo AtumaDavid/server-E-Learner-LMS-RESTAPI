dcbc01dccd9eb595c7fc0de0ce8206c622c95121
Atuma David <atumadavid35@gmail.com>
2024-12-21 07:27:41 +0100

    corrected logout bug, delete user token from redis and added middleware

--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -5,6 +5,7 @@ import {
   logoutUser,
   registerUser,
 } from "../controllers/user.controller";
+import { isAuthenticated } from "../middleware/auth";
 const userRouter = express.Router();
 
 userRouter.post("/registration", registerUser);
@@ -13,6 +14,6 @@ userRouter.post("/activate-user", activateUser);
 
 userRouter.post("/login", loginUser);
 
-userRouter.get("/logout", logoutUser);
+userRouter.get("/logout", isAuthenticated, logoutUser);
 
 export default userRouter;
